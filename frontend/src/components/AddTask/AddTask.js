4eb92cbe7c36abb6b3f68428cd92b8e23fa52a67
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2021-02-10 09:29:45 -0500

    mod: formsubmit event

--- frontend/src/components/AddTask/AddTask.js
+++ frontend/src/components/AddTask/AddTask.js
@@ -1,15 +1,26 @@
 import Style from './AddTask.module.css'
+import { FaPlusSquare } from 'react-icons/fa'
+import { IconContext } from 'react-icons';
 
 const AddTask = ({ addTask }) => {
-    const formSubmit = (e) => {
-        e.preventDefault()
-        addTask(e.target.newtask.value)
+
+    const formSubmit = () => {
+        const form = document.getElementById('addform');
+        if(form.newtask.value != ""){
+            addTask(form.newtask.value)
+            form.newtask.value = ""
+        }
     }
-    
+
     return (
-        <form onSubmit={formSubmit}>
-            <input type="text" name="newtask" />
-            <input type="submit"/>
+        <form className={ Style.form } id="addform">
+            <input type="text" name="newtask" className={Style.inputField}/>
+            <IconContext.Provider value={{ className:"addIcon" }}>
+                <FaPlusSquare 
+                    className={Style.addIcon}
+                    onClick={formSubmit}
+                />
+            </IconContext.Provider>
         </form>
     )
 }
