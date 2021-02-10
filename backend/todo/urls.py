15eadfd935f37cf566fdd3082f061af8e4b68b74
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2021-02-10 07:51:07 -0500

    init

---
+++ backend/todo/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+   path('tasks/', views.TaskList.as_view(), name='tasks'),
+   path('tasks/<int:pk>/', views.TaskDetail.as_view(), name='task'), 
+]
